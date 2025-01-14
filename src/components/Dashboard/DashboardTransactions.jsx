d6578b399ddf1543ff44df68d5d5b59615817e49
jerryemmanuel <jerryemmanuelolisa@gmail.com>
2025-01-14 11:29:17 +0100

    feat: transaction table completed

---
+++ src/components/Dashboard/DashboardTransactions.jsx
@@ -0,0 +1,12 @@
+import TransactionTable from "./TransactionTable"
+
+const DashboardTransactions = () => {
+  return (
+    <section className="mt-10">
+      <h2 className="font-semibold text-lg font-inter text-dark">Latest Transactions</h2>
+      <TransactionTable />
+    </section>
+  )
+}
+
+export default DashboardTransactions
--- src/pages/Dashboard/Dashborad/Dashboard.jsx
+++ src/pages/Dashboard/Dashborad/Dashboard.jsx
@@ -1,10 +1,12 @@
 import DashboardInfo from "../../../components/Dashboard/DashboardInfo"
+import DashboardTransactions from "../../../components/Dashboard/DashboardTransactions"
 
 const Dashboard = () => {
   return (
-    <section className="px-6 w-full">
+    <section className="px-6 w-full lg:w-full overflow-hidden">
       <h2 className="font-poppins font-semibold text-dark text-xl lg:text-2xl">Dashboard</h2>
       <DashboardInfo />
+      <DashboardTransactions />
     </section>
   )
 }
