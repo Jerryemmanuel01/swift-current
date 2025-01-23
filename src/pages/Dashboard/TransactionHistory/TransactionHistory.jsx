231ecc7dac0edcebfc18759c4f586100112f942f
jerryemmanuel <jerryemmanuelolisa@gmail.com>
2025-01-23 22:43:30 +0100

    fix: Transaction history fix

--- src/pages/Dashboard/TransactionHistory/TransactionHistory.jsx
+++ src/pages/Dashboard/TransactionHistory/TransactionHistory.jsx
@@ -1,8 +1,7 @@
 import { useSelector } from "react-redux";
 import TransactionHistoryTable from "../../../components/Dashboard/TransactionHistoryTable";
-e
 const TransactionHistory = () => {
-    const { user } = useSelector((state) => state.userInfo);
+  const { user } = useSelector((state) => state.userInfo);
   return (
     <section className="px-6 w-full lg:w-full overflow-hidden">
       <h2 className="font-semibold text-lg font-inter text-dark">
