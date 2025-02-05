8de5740e0ed928b93ae19ac09ac5cd34459206a1
jerryemmanuel <jerryemmanuelolisa@gmail.com>
2025-02-05 16:20:37 +0100

    Feat: token added

---
+++ src/hooks/dashboardHooks/useTokenForm.js
@@ -0,0 +1,105 @@
+import { useFormik } from "formik";
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { toast } from "react-hot-toast";
+import { funding, reset } from "../../services/features/funding/fundingSlice";
+import { useNavigate } from "react-router-dom";
+import { fundingSchema } from "../../lib/schema";
+import { walletAddresses } from "../../lib/utils";
+import { fetchUserInfo } from "../../services/features/userInfo/userInfoSlice";
+
+const useTokenForm = () => {
+  const [copied, setCopied] = useState(false);
+  const [qrCode, setQrCode] = useState("");
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { isLoading, isError, message, isSuccess } = useSelector(
+    (state) => state.funding
+  );
+
+  useEffect(() => {
+    if (isError) toast.error(message);
+    if (isSuccess) {
+      toast.success("Transaction Processing... please wait");
+      formik.resetForm();
+      dispatch(fetchUserInfo());
+      navigate("/dashboard");
+    }
+    dispatch(reset());
+    return;
+  }, [isSuccess, isError, message]);
+
+  const formik = useFormik({
+    initialValues: {
+      amount: "3000",
+      blockchainNetwork: "",
+      walletAddress: "",
+      transactionId: "",
+    },
+    validationSchema: fundingSchema,
+    onSubmit: ({
+      amount,
+      blockchainNetwork,
+      transactionId,
+      walletAddress,
+    } = values) => {
+      if (formik.values.blockchainNetwork === "") {
+        toast.error("Blockchain Network is required");
+        return;
+      }
+      const metadata = {
+        blockchainNetwork,
+        walletAddress,
+        transactionId,
+      };
+      const userData = { amount, metadata };
+      dispatch(funding(userData));
+    },
+  });
+
+  const handleCopy = () => {
+    if (formik.values.walletAddress) {
+      navigator.clipboard
+        .writeText(formik.values.walletAddress)
+        .then(() => {
+          setCopied(true);
+          toast.success("Wallet address copied");
+          setTimeout(() => setCopied(false), 2000); // Reset after 2 seconds
+        })
+        .catch((error) => {
+          toast.error("Failed to copy text: ", error);
+        });
+      return;
+    }
+    toast.error("Please select a blockchain network");
+  };
+
+  const findAddressByNetwork = (blockchainNetwork) => {
+    return walletAddresses.find(
+      (wallet) => wallet.blockchainNetwork === blockchainNetwork
+    )?.walletAddress;
+  };
+  const findQRCodeByNetwork = (blockchainNetwork) => {
+    setQrCode(
+      walletAddresses.find(
+        (qrCode) => qrCode.blockchainNetwork === blockchainNetwork
+      )?.qrCode
+    );
+  };
+
+  return {
+    formik,
+    handleCopy,
+    copied,
+    findAddressByNetwork,
+    findQRCodeByNetwork,
+    walletAddresses,
+    qrCode,
+    setQrCode,
+    isLoading,
+  };
+};
+
+export default useTokenForm;
--- src/pages/Dashboard/Token/Token.jsx
+++ src/pages/Dashboard/Token/Token.jsx
@@ -1,5 +1,23 @@
+import TokenImg from "../../../assets/Images/swift-token.png";
+import TokenForm from "../../../components/Dashboard/TokenForm";
+
 const Token = () => {
-  return <div>Token</div>;
+  return (
+    <section className="px-6 mb-8 w-full md:max-w-[670px] mx-auto">
+      <h2 className="font-poppins font-semibold text-dark text-xl lg:text-2xl">
+        Token
+      </h2>
+      <div className="mt-4">
+        <img src={TokenImg} alt="token" className="" loading="lazy" />
+      </div>
+      <div className="mt-4">
+        <h3 className="font-merriweather font-bold text-center text-primary">
+          Purchase Token
+        </h3>
+        <TokenForm />
+      </div>
+    </section>
+  );
 };
 
 export default Token;
