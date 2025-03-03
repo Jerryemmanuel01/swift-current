b7c5c52cf6ee27b43bcc204a3b8c1c66345f68ca
jerryemmanuel <jerryemmanuelolisa@gmail.com>
2025-03-03 09:46:04 +0100

    fix: Helmet provider added

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import { store } from "./store";
 import Loader from "./components/General/Loader.jsx";
 import { Toaster } from "react-hot-toast";
 import ErrorBoundary from "./components/General/ErrorBoundary.js";
+import { HelmetProvider } from "react-helmet-async";
 
 createRoot(document.getElementById("root")).render(
   <Suspense fallback={<Loader />}>
@@ -22,7 +23,9 @@ createRoot(document.getElementById("root")).render(
           </div>
         }
       >
-        <App />
+        <HelmetProvider>
+          <App />
+        </HelmetProvider>
       </ErrorBoundary>
     </Provider>
   </Suspense>
