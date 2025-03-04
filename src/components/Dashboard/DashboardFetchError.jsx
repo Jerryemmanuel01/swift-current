e2aa8fe021d88468b0dc191af7ac9e50c1f28cc9
jerryemmanuel <jerryemmanuelolisa@gmail.com>
2025-03-04 21:20:50 +0100

    feat: verify email error fixed

--- src/components/Dashboard/DashboardFetchError.jsx
+++ src/components/Dashboard/DashboardFetchError.jsx
@@ -1,9 +1,11 @@
+import { useNavigate } from "react-router-dom";
 
 const DashboardFetchError = ({
   setRetry,
   title = "Error fetching your details",
   logoutBtn
 }) => {
+  const navigate = useNavigate()
   return (
     <div className="w-full h-screen text-center flex items-center justify-center flex-col gap-3 ">
       <p className="text-xl font-semibold font-merriweather">
@@ -16,7 +18,7 @@ const DashboardFetchError = ({
       >
         Retry
       </button>
-      <button name="button" className="mt-2 font-merriweather border-b font-semibold text-secondary" onClick={()=>logoutBtn()}>
+      <button name="button" className="mt-2 font-merriweather border-b font-semibold text-secondary" onClick={()=>{logoutBtn ? logoutBtn() : navigate("home")}}>
         Back Home
       </button>
     </div>
--- src/pages/Dashboard/Verify/Verify.jsx
+++ src/pages/Dashboard/Verify/Verify.jsx
@@ -1,23 +1,18 @@
-import useVerifyEmail from "../../../hooks/dashboardHooks/useVerifyEmail";
-import DashboardFetchError from "../../../components/Dashboard/DashboardFetchError";
-import Loader from "../../../components/General/Loader";
-import Header from "../../../layouts/AuthLayout/Header";
-import useLogout from "../../../hooks/dashboardHooks/useLogout";
+import useVerifyEmail from '../../../hooks/dashboardHooks/useVerifyEmail'
+import DashboardFetchError from '../../../components/Dashboard/DashboardFetchError';
+import Loader from '../../../components/General/Loader';
+import Header from '../../../layouts/AuthLayout/Header';
 
 const Verify = () => {
   const { isError, isVerifyEmailLoading, setRetry } = useVerifyEmail();
   if (isVerifyEmailLoading) return <Loader title="Verifying Email" />;
-  const { logoutBtn } = useLogout();
-  if (isError)
-    return (
-      <DashboardFetchError setRetry={setRetry} title="Verification failed" logoutBtn={logoutBtn}/>
-    );
+  if (isError) return <DashboardFetchError setRetry={setRetry} title="Verification failed" />;
 
   return (
-    <section className="">
+    <section className=''>
       <Header />
     </section>
-  );
-};
+  )
+}
 
-export default Verify;
+export default Verify
